4ef6b05bf873b7454127c1d0c5340c91bbb4f836
maxbob22 <maximbobkov1@gmail.com>
2022-02-28 18:11:01 -0500

    Please

--- Mid Year/Assets/Scripts/RevolverProjectile.cs
+++ Mid Year/Assets/Scripts/RevolverProjectile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RevolverProjectile : MonoBehavior
+public class RevolverProjectile : MonoBehaviour
 {
     public ParticleSystem particleSystem;
 
